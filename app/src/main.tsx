0da9f821832c866a81e7d39628bb58eb255c937b
koji.iwase <koji.iwase@prtimes.co.jp>
2024-05-13 15:21:33 +0900

    suspense処理の追加

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,17 +1,26 @@
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { App } from "./App";
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Suspense fallback={<div>Loading...</div>}>
+        {" "}
+        <App />
+      </Suspense>
     </QueryClientProvider>
   </StrictMode>
 );
